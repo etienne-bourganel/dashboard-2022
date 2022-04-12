f1db602603bcd9e7c769a6f69992abe1ce0c4ae6
Etienne Bourganel <etienne.bourganel@skymill.io>
2022-04-12 16:08:28 +0200

    Change project structure

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ app.get('/api', (req, res) => {
     res.json({message: "Hello from server!"})
 })
 
+// app.get()
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 })
